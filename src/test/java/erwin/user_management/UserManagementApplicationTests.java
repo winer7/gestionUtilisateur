fb8f9b2dc47da00cd1197aa3b4ce85948468fcda
winer7 <70219756+winer7@users.noreply.github.com>
2025-02-03 09:53:07 +0000

    Premier commit de mon projet

---
+++ src/test/java/erwin/user_management/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package erwin.user_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
