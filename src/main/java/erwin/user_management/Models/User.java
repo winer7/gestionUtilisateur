c8226a80289a357c42b791432bb51fd18ab94a45
winer7 <70219756+winer7@users.noreply.github.com>
2025-03-14 15:07:50 +0000

    leR ajout controlleur authentification

--- src/main/java/erwin/user_management/Models/User.java
+++ src/main/java/erwin/user_management/Models/User.java
@@ -1,5 +1,8 @@
 package erwin.user_management.Models;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,8 +14,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max=200)
     private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 120)
     private String password;
+
     private String role; // ROLE_USER, ROLE_ADMIN
 }
--- src/main/java/erwin/user_management/UserManagementApplication.java
+++ src/main/java/erwin/user_management/UserManagementApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(UserManagementApplication.class, args);}
 
 }
